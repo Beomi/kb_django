cb6a69a7eae2f5e572dd11be6c008f966ad7a1d3
Beomi <latheledusjp@gmail.com>
2017-09-11 03:55:36 +0900

    Asia/Seoul

--- kb_django/settings.py
+++ kb_django/settings.py
@@ -106,13 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
+TIME_ZONE = 'Asia/Seoul'
 
 
 # Static files (CSS, JavaScript, Images)
